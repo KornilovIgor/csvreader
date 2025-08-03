d85df228e4fa6488318ae1247d9fce491e37c175
lomamord <mail@kote.dev>
2025-08-03 21:48:41 +0300

    develop: Added test: Division by zero.

--- tests/evaluator_test.cpp
+++ tests/evaluator_test.cpp
@@ -19,3 +19,17 @@ TEST(EvaluatorTest, BasicAddition) {
     EXPECT_EQ(table[3][2], "1");   // Ячейка B30: =B1 + A1 = 0 + 1 = 1
     EXPECT_EQ(err.str(), "");
 }
+
+TEST(EvaluatorTest, DivisionByZero) {
+    Table table = {
+        {"", "A", "B"},
+        {"1", "10", "0"},
+        {"2", "=A1/B1", "5"}
+    };
+
+    std::ostringstream err;
+    bool ok = evaluate_table(table, err);
+
+    EXPECT_FALSE(ok);
+    EXPECT_NE(err.str().find("Division by zero"), std::string::npos);
+}
